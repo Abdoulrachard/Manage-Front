4bd1e4210291e8b8ae9d8b87a25b47b161273a6a
Abdoulrachard <Abdoulrachard@gmail.com>
2024-04-01 04:37:26 +0200

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite' 
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'; 
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,15 +8,36 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': new URL('./src', import.meta.url).pathname
     }
   },
-    server: {
-      cors: {
-        origin: 'https://cche-ch.com', // Remplacez par l'origine de votre application Vue.js
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        allowedHeaders: ['Content-Type', 'Authorization'],
-      },
+  server: {
+    cors: {
+      origin: 'https://cche-ch.com', // Remplacez par l'origine de votre application Vue.js
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
     },
-})
+  },
+});
+// import { fileURLToPath, URL } from 'node:url'
+// import { defineConfig } from 'vite' 
+// import vue from '@vitejs/plugin-vue'
+
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   },
+//     server: {
+//       cors: {
+//         origin: 'https://cche-ch.com', // Remplacez par l'origine de votre application Vue.js
+//         methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//         allowedHeaders: ['Content-Type', 'Authorization'],
+//       },
+//     },
+// })
 
