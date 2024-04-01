15c79d7be061fca9ddfe7e652ea18dbd5651c066
Abdoulrachard <Abdoulrachard@gmail.com>
2024-04-01 05:20:24 +0200

    fix bug in projectview

--- src/views/projects/ProjectView.vue
+++ src/views/projects/ProjectView.vue
@@ -1,6 +1,6 @@
 <script setup>
     import ProjectRow from '@/components/projects/ProjectRow.vue';
-    import ArrowLeft from '@/components/layout/Arrowleft.vue';
+    import ArrowLeft from '../../components/layout/ArrowLeft.vue';
 </script>
 
 <template>
