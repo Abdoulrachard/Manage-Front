ffa9d3cfe827ea7e1d2eaf2a16e2aee004bad060
Abdoulrachard <Abdoulrachard@gmail.com>
2024-04-02 15:09:39 +0100

    max-height for card detail img

--- src/components/actualities/ActualitiesShow.vue
+++ src/components/actualities/ActualitiesShow.vue
@@ -135,8 +135,8 @@ a {
     -webkit-text-decoration-skip: objects;
 }
 .embed-responsive__item{
-    max-height: 100%;
-    max-width: 100%;
+    height: 100%;
+    width: 100%;
     position: absolute;
     top: 0;
     left: 0;
