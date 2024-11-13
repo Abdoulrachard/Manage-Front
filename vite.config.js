c39d2f1530a3345288aaeeae89fb3d1f13f4fa3e
Abdoulrachard <abdoulrachard@gmail.com>
2024-11-13 12:45:50 +0100

    added base route into config file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/',
   plugins: [
     vue(),
   ],
@@ -14,7 +15,7 @@ export default defineConfig({
   },
     server: {
       cors: {
-        origin: 'https://cche-ch.com',
+        origin: 'https://cche-ch.org',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization'],
       },
